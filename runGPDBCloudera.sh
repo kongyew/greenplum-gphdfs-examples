71e34b21f37db3c21f91d585b8e29ef86c1b6813
kongyew <kongyew@gmail.com>
2018-05-07 16:32:43 -0700

    updated

--- runGPDBCloudera.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Dockerfiles are located under `docker` directory
-docker-compose -f docker/docker-compose.yml up -d
-
-
-# use this command to rebuild docker images if there are some changes
-#docker-compose -f docker/docker-compose.yml up --build
